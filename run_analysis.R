80e665ef91fccd2c5ec6ed8f26718db59d1bb469
Jignesh <jig2nesh@gmail.com>
2015-04-27 07:43:43 +1000

    This is the run_analysis.R (for peer assessment)

---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+r_Data <- function () {
+  
+  path_rf <- file.path("./data" , "UCI HAR Dataset")
+  files<-list.files(path_rf, recursive=TRUE)
+  
+  #Print all the files avaialable in data folder
+  files
+  
+  dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+  dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+  
+  
+  dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+  dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+  
+  dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+  dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+  
+  dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+  dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+  dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+  
+  names(dataSubject)<-c("subject")
+  names(dataActivity)<- c("activity")
+  dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+  names(dataFeatures)<- dataFeaturesNames$V2
+  
+  dataCombine <- cbind(dataSubject, dataActivity)
+  Data <- cbind(dataFeatures, dataCombine)
+  
+  
+  
+  subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+  selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+  Data<-subset(Data,select=selectedNames)
+  
+  
+  activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+  
+  names(Data)<-gsub("^t", "time", names(Data))
+  names(Data)<-gsub("^f", "frequency", names(Data))
+  names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+  names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+  names(Data)<-gsub("Mag", "Magnitude", names(Data))
+  names(Data)<-gsub("BodyBody", "Body", names(Data))
+  
+  library(plyr);
+  Data2<-aggregate(. ~subject + activity, Data, mean)
+  Data2<-Data2[order(Data2$subject,Data2$activity),]
+  write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+  
+  
+  #library(knitr)
+  #knit2html("codebook.Rmd")
+  
+  require(knitr) # required for knitting from rmd to md
+  require(markdown) # required for md to html 
+  knit('codebook.Rmd', 'codebook.md') # creates md file
+  markdownToHTML('codebook.md', 'test.html') # creates html file
+  
+  
+}
